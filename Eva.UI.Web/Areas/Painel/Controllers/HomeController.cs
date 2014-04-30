02fd718fc112382e2b4c9fb083e41f44bd76ed24
Cleyton Ferrari <cleyton@w7br.com>
2014-04-30 18:00:49 -0400

    adicionado helper para FlashMensenger

--- Eva.UI.Web/Areas/Painel/Controllers/HomeController.cs
+++ Eva.UI.Web/Areas/Painel/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 using Microsoft.AspNet.Identity;
@@ -20,23 +21,41 @@ namespace Eva.UI.Web.Areas.Painel.Controllers
         [AllowAnonymous]
         public ActionResult Login(LoginViewModel usuario)
         {
-            if (usuario.Login != "cleyton") return View(usuario);
-
-            var claims = new List<Claim>
+            if (Request.HttpMethod == "POST")
             {
-                new Claim(ClaimTypes.Name, "Cleyton"),
-                new Claim(ClaimTypes.Email, "cleyton@email.com"),
-                new Claim(ClaimTypes.Role, "Admin"),
-                new Claim(ClaimTypes.Role, "Master")
-            };
+                if (usuario.Login == "cleyton")
+                {
+                    
 
-            var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+                    var claims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, "Cleyton"),
+                        new Claim(ClaimTypes.Email, "cleyton@email.com"),
+                        new Claim(ClaimTypes.Role, "Admin"),
+                        new Claim(ClaimTypes.Role, "Master")
+                    };
 
+                    var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+
+                    var ctx = Request.GetOwinContext();
+                    var authenticationManager = ctx.Authentication;
+                    authenticationManager.SignIn(new AuthenticationProperties {IsPersistent = usuario.Lembrar}, identity);
+
+                    return RedirectToAction("Index");
+                }
+
+                this.Flash("Dados de acesso não são válidos!", FlashEnum.Error);
+            }
+
+            return View(usuario);
+        }
+
+        public ActionResult Sair()
+        {
             var ctx = Request.GetOwinContext();
             var authenticationManager = ctx.Authentication;
-            authenticationManager.SignIn(new AuthenticationProperties { IsPersistent = usuario.Lembrar }, identity);
-
-            return RedirectToAction("Index");
+            authenticationManager.SignOut();
+            return RedirectToAction("Login");
         }
     }
 
---
+++ Eva.UI.Web/Helpers/FlashHelper.cs
@@ -0,0 +1,29 @@
+﻿namespace System.Web.Mvc
+{
+    public enum FlashEnum
+    {
+        Success = 1,
+        Info = 2,
+        Warning = 3,
+        Error = 4
+    }
+    public static class FlashHelper
+    {
+        public static void Flash(this Controller controller, string message, FlashEnum type = FlashEnum.Success)
+        {
+            var classe = "";
+            switch (type)
+            {
+                case FlashEnum.Success: classe = "alert-success";
+                    break;
+                case FlashEnum.Info: classe = "alert-info";
+                    break;
+                case FlashEnum.Warning: classe = "alert-warning";
+                    break;
+                case FlashEnum.Error: classe = "alert-danger";
+                    break;
+            }
+            controller.TempData[classe] = message;
+        }
+    }
+}
