ac277a7d0f2de6ccb81da9b28e6b31700e6b4ae0
Cleyton Ferrari <cleyton@w7br.com>
2014-04-30 17:07:07 -0400

    adicionado um exemplo de login, usanco claims

--- Eva.UI.Web/Areas/Painel/Controllers/HomeController.cs
+++ Eva.UI.Web/Areas/Painel/Controllers/HomeController.cs
@@ -1,23 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 
 namespace Eva.UI.Web.Areas.Painel.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
-        //
-        // GET: /Painel/Home/
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Login()
+        [AllowAnonymous]
+        public ActionResult Login(LoginViewModel usuario)
         {
-            return View();
+            if (usuario.Login != "cleyton") return View(usuario);
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, "Cleyton"),
+                new Claim(ClaimTypes.Email, "cleyton@email.com"),
+                new Claim(ClaimTypes.Role, "Admin"),
+                new Claim(ClaimTypes.Role, "Master")
+            };
+
+            var identity = new ClaimsIdentity(claims, DefaultAuthenticationTypes.ApplicationCookie);
+
+            var ctx = Request.GetOwinContext();
+            var authenticationManager = ctx.Authentication;
+            authenticationManager.SignIn(new AuthenticationProperties { IsPersistent = usuario.Lembrar }, identity);
+
+            return RedirectToAction("Index");
         }
-	}
+    }
+
+    public class LoginViewModel
+    {
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public bool Lembrar { get; set; }
+    }
 }
---
+++ Eva.UI.Web/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
+
+[assembly: OwinStartup(typeof(Eva.UI.Web.Startup))]
+
+namespace Eva.UI.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+                LoginPath = new PathString("/Painel/Home/Login")
+            });
+        }
+    }
+}
