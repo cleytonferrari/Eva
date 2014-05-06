7981ff4ada764a80febd98e5574243ee2242908a
Cleyton Ferrari <cleyton@w7br.com>
2014-05-06 11:54:26 -0400

    adicionado o HTML da listagens dos arquivos, com sortable

---
+++ Eva.UI.Web/Areas/Painel/Controllers/ArquivosController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eva.UI.Web.Areas.Painel.Controllers
+{
+    public class ArquivosController : Controller
+    {
+        //
+        // GET: /Painel/Arquivos/
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
