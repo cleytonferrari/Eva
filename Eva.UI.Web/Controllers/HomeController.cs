02fc222abcdaa4f966e66b4be966803d20258c4e
Cleyton Ferrari <cleyton@w7br.com>
2014-05-21 11:45:27 -0400

    Marcação da pagina inicial;

--- Eva.UI.Web/Controllers/HomeController.cs
+++ Eva.UI.Web/Controllers/HomeController.cs
@@ -16,6 +16,12 @@ namespace Eva.UI.Web.Controllers
             vm.Noticias.Urgente = Fabrica.NoticiaAplicacaoMongo().ListarPublicadas("Urgente").FirstOrDefault()?? new Noticia();
             vm.Noticias.Destaques = Fabrica.NoticiaAplicacaoMongo().ListarPublicadas("Destaque").Take(5) ?? new List<Noticia>();
             vm.Noticias.AoLadoDoDestaque = Fabrica.NoticiaAplicacaoMongo().ListarPublicadas("Ao lado do destaque").Take(4) ?? new List<Noticia>();
+            vm.Noticias.EmbaixoDoDestaque = Fabrica.NoticiaAplicacaoMongo().ListarPublicadas("Embaixo do destaque").Take(5) ?? new List<Noticia>();
+            vm.Noticias.Centro = Fabrica.NoticiaAplicacaoMongo().ListarPublicadas("Centro").Take(6) ?? new List<Noticia>();
+            vm.Noticias.Categoria01 = Fabrica.NoticiaAplicacaoMongo().ListarPublicadas("Categoria 01").Take(3) ?? new List<Noticia>();
+            vm.Noticias.Categoria02 = Fabrica.NoticiaAplicacaoMongo().ListarPublicadas("Categoria 02").Take(3) ?? new List<Noticia>();
+            vm.Noticias.Categoria03 = Fabrica.NoticiaAplicacaoMongo().ListarPublicadas("Categoria 03").Take(3) ?? new List<Noticia>();
+
             return View(vm);
         }
     }
@@ -35,5 +41,10 @@ namespace Eva.UI.Web.Controllers
         public Noticia Urgente { get; set; }
         public IEnumerable<Noticia> Destaques { get; set; }
         public IEnumerable<Noticia> AoLadoDoDestaque { get; set; }
+        public IEnumerable<Noticia> EmbaixoDoDestaque { get; set; }
+        public IEnumerable<Noticia> Centro { get; set; }
+        public IEnumerable<Noticia> Categoria01 { get; set; }
+        public IEnumerable<Noticia> Categoria02 { get; set; }
+        public IEnumerable<Noticia> Categoria03 { get; set; }
     }
 }
