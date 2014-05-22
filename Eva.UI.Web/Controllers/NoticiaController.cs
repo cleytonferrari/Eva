7dbcc4c35d2244b05de749f5afb968df5febc0b7
Cleyton Ferrari <cleyton@w7br.com>
2014-05-22 17:19:42 -0400

    Criado a view para listar as noticias

---
+++ Eva.UI.Web/Controllers/NoticiaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Eva.UI.Web.Controllers
+{
+    public class NoticiaController : Controller
+    {
+        // GET: Noticia
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
